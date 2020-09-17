4a96635328a9d5f7040cdfb4f1a3223a884c9816
Marcell <marcell.alves@gmail.com>
2020-09-17 09:21:53 -0300

    novas solucoes de reverse string e palindrome

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,15 +7,43 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+  const reversed = str.split('').reduce((rev, char) => char + rev, '')
+
+  return reversed
+}
+
+module.exports = reverse;
+
+// function reverse(str) {
+//   let reversed = ''
+
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+
+//   return reversed
+// }
+
+// function reverse(str) {
+//   let reversed = ''
+
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+
+//   return reversed
+// }
+
+// function reverse(str) {
+//   const revStr = str.split('').reverse().join('')
+
+//   return revStr
+// }
+
+//function reverse(str) {
   //let revStr = ''
   //for (var i = str.length - 1; i >= 0; i--) {
   //revStr += str[i]
   //}
   //return revStr
-
-  const revStr = str.split('').reverse().join('')
-
-  return revStr
-}
-
-module.exports = reverse;
+//}
